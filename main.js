1f08ec43ad4efebe1d92e7a214daaf5ee2409cd5
EmmaM-W <133997936+EmmaM-W@users.noreply.github.com>
2023-05-25 23:58:10 +0100

    feat: add button that prompts user for grid size

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 const gridContainer = document.querySelector('#gridContainer');
+const btn = document.querySelector('#gridButton');
 const div = document.createElement('div');
 div.style.backgroundColor= 'red';
-div.textContent = 'grid';
-div.addEventListener('onmouseover', function (e) {
-    e.target.style.backgroundColor = 'blue';
-});
+
+btn.addEventListener('click',() => {
+    do {
+        x = prompt('please enter the size of the grid you want. Must be between 1 and 100.');
+
+    } while (x < 1 || x > 100 || isNum(x) === false);
+    createGrid(x);
+})
+
+function isNum(input){
+    let isnum = /^\d+$/.test(input);
+    return isnum;
+}
 
 function createGrid(gridSize){
+    //first clear current grid
+    gridContainer.textContent = '';
     let gridHeight =  480/gridSize;
     console.log(gridHeight);
     //Creates copies of individual grid divs and add them to the container
     for (let index = 0; index < gridSize*gridSize; index++) {
         var grid = div.cloneNode(true);
-        grid.textContent = index;
         grid.style.flexShrink = '0';
         grid.style.height =  gridHeight +'px';
         grid.style.width = gridHeight + 'px';
@@ -25,4 +36,4 @@ function createGrid(gridSize){
     }
 }
 
-createGrid(8);
+
