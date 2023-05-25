12f400e982c9fb411c0435eb64133b1ea656cde3
EmmaM-W <133997936+EmmaM-W@users.noreply.github.com>
2023-05-25 23:04:17 +0100

    feat: grid elements now wrap correctly

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ div.style.backgroundColor= 'red';
 div.textContent = 'grid';
 
 function createGrid(gridSize){
+    let gridHeight =  480/gridSize;
+    console.log(gridHeight);
     //Creates copies of individual grid divs and add them to the container
-    for (let index = 0; index < gridSize; index++) {
+    for (let index = 0; index < gridSize*gridSize; index++) {
         var grid = div.cloneNode(true);
+        grid.textContent = index;
+        grid.style.flexShrink = '0';
+        grid.style.height =  gridHeight +'px';
+        grid.style.width = gridHeight + 'px';
+
         gridContainer.appendChild(grid);
     }
 }
